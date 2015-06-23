08adf94026b863f583e0b4c9b018d6f35d79fe5e
Łukasz Ławniczak <lawniczakl@student.mini.pw.edu.pl>
2015-06-24 00:53:12 +0200

    syfny commit

--- NESEMU.java
+++ NESEMU.java
@@ -15,6 +15,7 @@ public class NESEMU {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+		//syf
             System.out.printf("Hello user!");
 // TODO code application logic here
     }
